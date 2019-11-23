eb635b1c1b6e4b449a901c06a1392a65c45d8fe7
nickdem01 <46574657+nickdem01@users.noreply.github.com>
2019-11-22 18:48:52 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,21 @@ sort -u -o recs.txt recs.txt
 
 sort -u -o dates.txt dates.txt
 
+sort -u -o emails.txt emails.txt
+
+sort -u -o terms.txt terms.txt
+
 perl break.pl < dates.txt > datestemp.txt
 
 db_load -c duplicates=1 -T -f datestemp.txt -t btree da.idx
+
+perl break.pl < emails.txt > emailstemp.txt
+
+dbload -c duplicates=1 -T -f emailstemp.txt -t btree em.idx
+
+perl break.pl < terms.txt > termstemp.txt
+
+dbload -c duplicates=1 -T -f termstemp.txt -t btree te.idx
 
 perl break.pl < recs.txt > recstemp.txt
 
